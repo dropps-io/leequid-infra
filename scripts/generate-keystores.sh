bc9224c6d46e9eab42935a3e7ecc8df3a0444a37
bitbucket-pipelines <commits-noreply@bitbucket.org>
2023-06-18 16:24:16 +0200

    Update script generate-keystores.sh

--- scripts/generate-keystores.sh
+++ scripts/generate-keystores.sh
@@ -4,45 +4,48 @@
 # $LUKSO_KEY_GEN --language English existing-mnemonic --num_validators 1 --chain lukso  --validator_start_index 0 --keystore_password password --mnemonic="$(cat seed.txt)"
 
 LUKSO_KEY_GEN=./lukso-key-gen-cli
-KEYSTORE_SECRET=${KEYSTORE_SECRET:-"password"}
 MNEMONIC_FILE=${MNEMONIC_FILE:-"seed.txt"}
-VALIDATOR_PER_NODE=${VALIDATOR_PER_NODE:-1}
+VALIDATOR_PER_NODE=${VALIDATOR_PER_NODE:-2}
 NODE_COUNT=${NODE_COUNT:-2}
 NODE_PREFIX_NAME=${NODE_NAME:-"lukso"}
 NETWORK=${NETWORK:-"mainnet"}
 BUCKET=${BUCKET:-"leequid-prod-lukso"}
 SECRET_PREFIX=${SECRET_PREFIX:-"prod"}
 
+echo -e ">>> Get keystore secret from gcloud"
+keystore_secret_id=${SECRET_PREFIX}-${NETWORK}-${NODE_PREFIX_NAME}-keystore-secret
+KEYSTORE_SECRET=$(gcloud secrets versions access latest --secret="$keystore_secret_id")
+
+echo -e "\n>>> Iterate over [node_count=$NODE_COUNT]"
 for (( i=0; i<$NODE_COUNT; i++ )); do
   NODE_NAME=${NODE_PREFIX_NAME}-${i}
-  echo $NODE_NAME
+  echo -e "\n>>> Node Name [$NODE_NAME]"
 
-  ### Generate seed and keystore
+  echo -e "\n>>> Run [$LUKSO_KEY_GEN] Generate keystore and mnemonic [num_validators=$VALIDATOR_PER_NODE]"
   $LUKSO_KEY_GEN  --language English new-mnemonic --mnemonic_language English --chain lukso \
       --num_validators $VALIDATOR_PER_NODE \
       --keystore_password $KEYSTORE_SECRET \
       --mnemonic_file $MNEMONIC_FILE
 
-  ### Save mnemonic to Gcloud secret
   secret=${SECRET_PREFIX}-${NETWORK}-${NODE_NAME}-mnemonic
+  echo -e "\n>>> Save mnemonic file to gcloud secret [$secret]"
   gcloud secrets create $secret --data-file=$MNEMONIC_FILE
 
-  ### Copy deposit json to /tmp
+  echo -e "\n>>> Copy deposit to [/tmp/${NODE_NAME}-deposit.json]"
   cp validator_keys/deposit*.json /tmp/${NODE_NAME}-deposit.json
 
-  ### Save Keystore to bucket
-  #gsutil -m cp -r validator_keys/* gs://$BUCKET/$NETWORK/$NODE_NAME/keystores
+  echo -e "\n>>> Save keystores to [gs://$BUCKET/$NETWORK/$NODE_NAME/keystores]"
+  gsutil -m cp -r validator_keys/* gs://$BUCKET/$NETWORK/$NODE_NAME/keystores
 
-  ### Clean folders
   rm -rf validator_keys/ $MNEMONIC_FILE
 done
 
-### Merge deposit wallet json
+echo -e "\n>>> Merge all deposit wallet to [${NODE_PREFIX_NAME}-deposit-data.json]"
 jq -s 'add' /tmp/*deposit.json > ${NODE_PREFIX_NAME}-deposit-data.json
 
-### Save to bucket (gs://$BUCKET/$NETWORK/$NODE_NAME/${NODE_PREFIX_NAME}-deposit-data.json)
+echo -e "\n>>> Save deposit to [gs://$BUCKET/$NETWORK/${NODE_PREFIX_NAME}-deposit-data.json]"
+gsutil -m cp ${NODE_PREFIX_NAME}-deposit-data.json gs://$BUCKET/$NETWORK/
 
-### Clean /tmp/*deposit.json
 rm -f /tmp/*deposit.json
 
 
