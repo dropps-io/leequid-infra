e61d2bb7458e5e6ba1cef0b3eb072037e8cfcbd7
Axel <percenuage@users.noreply.github.com>
2023-06-27 17:35:14 +0200

    Add backend apps

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# deploy app nginx -n dev --dry-run
+### example: deploy app nginx -n dev --dry-run
 
 CHART=${1}
 APP=${2}
 ARGS=${@:3}
 
-helm upgrade --install nginx leequid-infra/charts/$CHART \
-  -f leequid-infra/charts/values/dev/$APP.values.yaml \
+dir=~/projects/dropps/leequid-infra/charts
+
+helm upgrade --install $APP $dir/$CHART \
+  -f $dir/values/dev/$APP.values.yaml \
   $ARGS
 
