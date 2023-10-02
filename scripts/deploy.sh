a23f45a2d3651dfb77aed0eabeefac6b720de56f
Axel <percenuage@users.noreply.github.com>
2023-10-02 23:43:00 +0200

    feat: migrate to the dev cluster

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,13 +2,17 @@
 
 ### example: deploy app nginx -n dev --dry-run
 
+ENV=dev
+
 CHART=${1}
 APP=${2}
 ARGS=${@:3}
 
 dir=leequid-infra/charts
 
-helm upgrade --install $APP $dir/$CHART \
-  -f $dir/values/dev/$APP.values.yaml \
+kubectl config use-context gke_leequid_europe-west1-c_leequid-$ENV
+
+helm upgrade -n main --install $APP $dir/$CHART \
+  -f $dir/values/$ENV/$APP.values.yaml \
   $ARGS
 
